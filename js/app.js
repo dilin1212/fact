e57054397827f71252ca8ef0ff803602a8cecc27
dilin <dilindinesan@gmail.com>
2017-04-13 21:31:41 +0300

    factorial

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var input = parseInt(prompt("Enter the number to get factorial of: "));
+var result = input;
+for (var i = 1; i < input; i++) {
+    result = i * result;
+}
+document.write(result);
+//# sourceMappingURL=app.js.map
---
+++ ts/factorial.ts
@@ -0,0 +1,7 @@
+
+var n = parseInt(prompt("Enter the number to get factorial of: "));
+var factorial = n;
+for(var i=1;i < n;i++){
+  factorial = i * factorial;
+}
+document.write();
